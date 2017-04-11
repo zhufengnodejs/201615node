fc314ed9db86efb2edd5151753edd039dc4cb12a
zhufengnodejs <zhufengnodejs@126.com>
2017-04-11 12:02:23 +0800

    babel本地全局安装和使用

--- 3.react/js/17.suggest.js
+++ 3.react/js/17.suggest.js
@@ -8,6 +8,7 @@ let Component = React.createClass({
   },
   handleChange(event){
     let wd = event.target.value;//获取关键字
+    //https://www.baidu.com/su?cb=jQuery_1493&wd=a&_=1491882359014
     $.ajax({
       url:'http://www.baidu.com/su',
       type:'GET',
@@ -15,23 +16,41 @@ let Component = React.createClass({
       data:{wd},//如果是get系请求，会把此对象序列化成字符串并放在url后面，如果是post系，会把请此对象变成查询字符中放在请求体里
       jsonp:'cb'//用于服务器端通过此参数名获取返回的方法名
     }).then((result) => {
+      // success then done 都是用于指定成功的回调函数
       this.setState({
+        index:-1,
         words:result.s
       });
     })
 
   },
+  handleKeyDown(event){
+    let code = event.keyCode;
+    if(code == 38 || code == 40){
+      let index = this.state.index;
+      if(code == 38){//上箭头
+        index--;
+        if(index<0)
+          index = this.state.words.length-1;
+      }else if(code == 40){//下箭头
+        index++;
+        if(index==this.state.words.length)
+          index = 0;
+      }
+      this.setState({index});
+    }
+  },
   render(){
     return (
       <div className="panel panel-default">
         <div className="panel-heading">
-          <input onChange={this.handleChange} type="text" className="form-control"/>
+          <input onKeyDown={this.handleKeyDown} onChange={this.handleChange} type="text" className="form-control"/>
         </div>
         <div className="panel-body">
           <ul className="list-group">
             {
               this.state.words.map((word,index)=>(
-                <li key={index} className="list-group-item">{word}</li>
+                <li key={index} className={"list-group-item "+(this.state.index == index?"active":"") }>{word}</li>
               ))
             }
           </ul>
