4ee0c3793037d3e7274c3c4995f14df89e23b1cf
zhufengnodejs <zhufengnodejs@126.com>
2017-04-11 20:07:57 +0800

    babel本地全局安装和使用

--- 5.webpack/myslider/Slider.js
+++ 5.webpack/myslider/Slider.js
@@ -1,8 +1,58 @@
 import React from 'react';
+require('./Slider.css');
 export default class Slider extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {index:0};
+  }
+  turn(n){
+    let index = this.state.index;
+    index += n;
+    if(index == this.props.images.length){
+      index = 0;
+    }else if(index ==-1){
+      index = this.props.images.length-1;
+    }
+    this.setState({index});
+  }
+  componentDidMount(){
+    this.autoPlay();
+  }
+  autoPlay(){
+    this.$timer = setInterval(()=>{
+      this.turn(1);
+    },2000);
+  }
   render(){
+    let {images} = this.props;
+    let style = {
+      width:images.length*200+'px',
+      left: this.state.index*-200+'px'
+    }
     return (
-      <div>轮播图</div>
+      <div className="slider-wrapper" onMouseOver={()=>clearInterval(this.$timer)} onMouseOut={()=>this.autoPlay()}>
+        <ul className="sliders" style={style}>
+          {
+            images.map((image,index)=>(
+              <li key={index} className="slider">
+                <img src={image.src}/>
+              </li>
+            ))
+          }
+
+        </ul>
+        <div className="arrows">
+          <span onClick={()=>this.turn(-1)} className="arrow arrow-left">&lt;</span>
+          <span onClick={()=>this.turn(1)}className="arrow arrow-right">&gt;</span>
+        </div>
+        <div className="slider-dots">
+          {
+            this.props.images.map((image,index)=>(
+              <span key={index} onClick={()=>this.turn(index-this.state.index)} className={"slider-dot "+(index == this.state.index?"active":"")}></span>
+            ))
+          }
+        </div>
+      </div>
     )
   }
 }
--- 5.webpack/myslider/index.js
+++ 5.webpack/myslider/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 import Slider from './Slider';
-
-ReactDOM.render(<Slider/>,document.querySelector('#app'));
+let images = [
+  {src:require('./images/1.jpg')},
+  {src:require('./images/2.jpg')},
+  {src:require('./images/3.jpg')},
+  {src:require('./images/4.jpg')}
+]
+ReactDOM.render(<Slider images={images}/>,document.querySelector('#app'));
