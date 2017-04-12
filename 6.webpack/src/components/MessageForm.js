35aa4561e9d6868a29f281d271371865fe3c07d4
zhufengnodejs <zhufengnodejs@126.com>
2017-04-12 12:08:22 +0800

    babel本地全局安装和使用

--- 6.webpack/src/components/MessageForm.js
+++ 6.webpack/src/components/MessageForm.js
@@ -1,23 +1,37 @@
 import React from 'react';
 /**
- * 1.为按钮绑定点击事件
+ * 1.为按钮绑定点击事件 给表单绑定提交事件
  * 2.把收集到的姓名和内容封装成一个对象，传递给MessageBox，从而修改状态。
  * 3.状态改变后会自动刷新 MessageList
  */
+// ES5 ES6 重大区别 es5所有事件处理函数 this=当前组件的实例
+// ES6 事件处理函数指向 null
 export default  class MessageForm extends React.Component{
-  render(){
+  constructor(){
+    super();
+  }
+  handleSubmit(event){
+    event.preventDefault();//阻止表单默认提交
+    let name = this.name.value;;
+    let content = this.content.value;
+
+
+  }
+  // react亲生 render方法里this都指向当前组件的实例
+  // 我们后生的 函数里this = undefined
+  render(){//render里面的this指向当前组件
     return (
-      <form className="form-horizontal">
+      <form className="form-horizontal" onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label htmlFor="name" className="control-label col-md-1">姓名</label>
           <div className="col-md-11">
-            <input id="name" type="text" className="form-control"/>
+            <input ref={ref=>{this.name=ref}} required="required" id="name" type="text" className="form-control"/>
           </div>
         </div>
         <div className="form-group">
           <label htmlFor="content" className="control-label col-md-1">内容</label>
           <div className="col-md-11">
-            <input type="text" id="content" className="form-control"/>
+            <textarea  cols="30" rows="10" className="form-control" id="content" required="required" ref={ref=>this.content=ref}></textarea>
           </div>
         </div>
         <div className="form-group">
