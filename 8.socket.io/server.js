98dba2619536bf4f7a07b32ebb4afa4a433a4a69
zhufengnodejs <zhufengnodejs@126.com>
2017-04-15 16:04:57 +0800

    babel本地全局安装和使用

--- 8.socket.io/server.js
+++ 8.socket.io/server.js
@@ -11,20 +11,35 @@ let server = require('http').createServer(app);
 //socket.io 是需要依赖http协议来建立连接的
 //io就是是socket.io在服务器端的实例了
 let io = require('socket.io')(server);
-
+let sockets = {};
 //监听客户端的连接 当有连接到来的时候执行回调函数
 io.on('connection',function(socket){
-   socket.send({author:'系统',content:'请输入呢称',createAt:new Date()});
+   socket.send({author:'系统',content:'请输入呢称',createAt:new Date().toLocaleString()});
    let username;
    socket.on('message',function (msg) {
       if(username){//不是第一次，
-        //把此消息发送给所有的客户端
-        io.emit('message',{author:username,content:msg,createAt:new Date()});
+        let regex = /@([^ ]+) (.+)/;
+        // exec match
+        let result = msg.match(regex);
+        //let result = regex.exec(msg);
+        if(result){//匹配上表示是私聊
+          let toUserName = result[1];//对方的用户名
+          let content = result[2];//内容
+          //给对方说话 作者就是自己
+          sockets[toUserName].send({author:username,content,createAt:new Date().toLocaleString()});
+        }else{//否则就是公聊
+              //把此消息发送给所有的客户端
+          io.emit('message',{author:username,content:msg,createAt:new Date().toLocaleString()});
+        }
+
       }else{
         //如果是第一次发言，那么会把此次的消息当成用户名
         username = msg;
-        io.emit('message',{author:'系统',content:`欢迎${username}光临聊天室`,createAt:new Date()});
+        //把用户名跟对应的socket建立关联
+        sockets[username] = socket;
+        io.emit('message',{author:'系统',content:`欢迎${username}光临聊天室`,createAt:new Date().toLocaleString()});
       }
+
    });
 });
 server.listen(8080);
@@ -42,8 +57,11 @@ server.listen(8080);
  *    1.当客户端初次连接的时候，先提示他输入呢称
  *    2. 当客户端第一次输入信息过来，会把此次的信息当成呢称。
  *    3. 以后客户端再发信息过来，就会显示是此呢称发的信息
- *
  * 3. 实现私聊
+ *    1. 可以点击用户名，点击的时候把那个人的用户名显示在输入框里。 @人名 xxxxx
+ *    2. 把此消息发给服务器
+ *    3. 服务器从消息取出你想私聊的用户名和内容
+ *    4. 向对方单发消息
  * 4. 聊天持久化
  * 5. 分房间聊天
  */
