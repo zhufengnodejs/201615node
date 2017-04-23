b09ef6d67570204e43ec030681ce4221e3f1629f
zhufengnodejs <zhufengnodejs@126.com>
2017-04-23 13:02:07 +0800

    2

--- 13.react-router/src/components/App.js
+++ 13.react-router/src/components/App.js
@@ -2,7 +2,24 @@ import React from 'react';
 export default class App extends React.Component{
    render(){
      return (
-       <div>App</div>
+       <div>
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              <div>
+                <ul className="nav navbar-nav">
+                  <li><a href="#/">首页</a></li>
+                  <li><a href="#/user">用户管理</a></li>
+                  <li><a href="#/profile">个人设置</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="row">
+              <div className="col-md-12">
+                  {this.props.children}
+              </div>
+          </div>
+       </div>
      )
    }
 }
--- 13.react-router/src/index.js
+++ 13.react-router/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
+require('bootstrap/dist/css/bootstrap.css');
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
+import Home from './components/Home.js';
+import Profile from './components/Profile.js';
+import User from './components/User.js';
+import {Router,Route,IndexRoute,hashHistory} from 'react-router';
 
-ReactDOM.render(<App/>,document.querySelector('#container'));
+ReactDOM.render(<Router history = {hashHistory}>
+    <Route path="/" component={App}>
+        <IndexRoute  component={Home}></IndexRoute>
+        <Route  path="user" component={User}></Route>
+        <Route  path="profile" component={Profile}></Route>
+    </Route>
+  </Router>,document.querySelector('#container'));
